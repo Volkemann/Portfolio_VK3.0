e8527b9abf2cdb1915b6d81b3a3fcd1c7aee8cb2
Vkzz <joojaovitor2742@gmail.com>
2024-07-23 22:07:57 -0300

    Portfolio_VK3.0
    Por enquanto este será meu portfolio por um tempo, daqui pra frente só aprimorar ele com novos projetos ou algo do tipo

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
-const menu = document.querySelector(".menu");
-const nav = document.querySelector(".nav");
+
+/* SCROLLAR TELA PARA BAIXO*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById('botao');
+    const sectionSobre = document.querySelector('.sobre');
+    if (botao && sectionSobre) {
+        botao.addEventListener('click', function() {
+            sectionSobre.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    }
+});
+
+/* SCROLLAR TELA PARA BAIXO*/
 
 
-menu.addEventListener("click", () => nav.classList.toggle
-("active"));
+/* BARRA DE NAVEGAÇÃO ROLAGEM*/
 
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header');
+    header.classList.toggle('rolagem',window.scrollY > 100);
+});
+
+/* BARRA DE NAVEGAÇÃO ROLAGEM*/
+
+
+/*EFEITO DE APARECER CONTEUDO*/
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+  
+  const elements = document.querySelectorAll('.hidden');
+  
+  elements.forEach((element) => myObserver.observe(element));
+  
+
+/*EFEITO DE APARECER CONTEUDO*/
